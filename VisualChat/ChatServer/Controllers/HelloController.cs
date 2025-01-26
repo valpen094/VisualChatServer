4ce7d83f6ae32ee5e042fa09e52891af33e82807
valpen094 <welciei3524@gmail.com>
2025-01-26 21:22:09 +0900

    add 簡単なREST通信を行うクライアントとサーバーを追加

---
+++ VisualChat/ChatServer/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from REST API!";
+        }
+    }
+
+}
---
+++ VisualChat/VisualChat/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace VisualChat
+{
+    public partial class MainWindow : Window
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void OnRequestApi(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string url = "http://localhost:5028/hello"; // APIのエンドポイント
+                string response = await _httpClient.GetStringAsync(url);
+                ResponseText.Text = response;
+            }
+            catch (Exception ex)
+            {
+                ResponseText.Text = "エラー: " + ex.Message;
+            }
+        }
+    }
+}
